0302b9335d5ac3781bf255274e509531ec9f1f6a
Thibaut Foussard <thibaut.foussard@tmrw.com>
2022-12-04 17:31:24 -0500

    perfect-gui 4.0.1

--- perfect-gui/examples/src/fullpage.js
+++ perfect-gui/examples/src/fullpage.js
@@ -1,10 +1,19 @@
 //import GUI from 'perfect-gui';
 import GUI from './perfect-gui/index.replica';
 
-let gui = new GUI({ name: 'test' });
-gui.button('a butn', () => {});
-gui.toggle({ name: 'test', value: true });
+let gui = new GUI({ name: 'test',  });
 
+let f = gui.folder({ name: 'folder', maxHeight: 500 });
 for (let i = 0; i < 100; i++) {
-    gui.button('Button ' + i, () => {});
+    f.button('Button ' + i, () => {});
+}
+
+let f2 = gui.folder({ name: 'folder2', maxHeight: 500 });
+for (let i = 0; i < 100; i++) {
+    f2.button('Button ' + i, () => {});
+}
+
+let f3 = gui.folder({ name: 'folder2', maxHeight: 500 });
+for (let i = 0; i < 100; i++) {
+    f3.button('Button ' + i, () => {});
 }
--- perfect-gui/examples/src/perfect-gui/styles.js
+++ perfect-gui/examples/src/perfect-gui/styles.js
@@ -301,7 +301,7 @@
         width: 100%;
         position: relative;
         background: #434343;
-        overflow: hidden;
+        overflow: auto;
         margin-bottom: 3px;
         display: flex;
         flex-wrap: wrap;
