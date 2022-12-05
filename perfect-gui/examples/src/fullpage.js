776080387579f502437b1ca5854a5dc4406dc97d
Thibaut Foussard <thibaut.foussard@tmrw.com>
2022-12-04 20:33:03 -0500

    perfect-gui 4.1.1

--- perfect-gui/examples/src/fullpage.js
+++ perfect-gui/examples/src/fullpage.js
@@ -2,18 +2,34 @@
 import GUI from './perfect-gui/index.replica';
 
 let gui = new GUI({ name: 'test',  });
+let data = { x: 0.5, y: 0.5 };
+let data2 = { x: 0.5, y: 0.5 };
 
-let f = gui.folder({ name: 'folder', maxHeight: 500 });
-for (let i = 0; i < 100; i++) {
+let f = gui.folder({ name: 'folder' });
+/* for (let i = 0; i < 10; i++) {
     f.button('Button ' + i, () => {});
-}
+} */
 
-let f2 = gui.folder({ name: 'folder2', maxHeight: 500 });
-for (let i = 0; i < 100; i++) {
+let f2 = f.folder({ name: 'subfolder'});
+/* for (let i = 0; i < 10; i++) {
     f2.button('Button ' + i, () => {});
-}
+} */
+f2.vector2({name: 'vector2', 
+    x: { obj: data, prop: 'x', min: -30, max: 30},
+    y: { obj: data, prop: 'y', min: -30, max: 30},
+}, () => {});
 
-let f3 = gui.folder({ name: 'folder2', maxHeight: 500 });
-for (let i = 0; i < 100; i++) {
-    f3.button('Button ' + i, () => {});
-}
+let f3 = f2.folder({ name: 'subfolder' });
+//for (let i = 0; i < 10; i++) {
+    /* f3.button('Button ', () => {});
+    
+    f3.toggle({ name: 'toggle', value: true}, () => {});
+    f3.slider({ name: 'slider', value: 0.5, min: 0, max: 1}, () => {});
+    f3.list({name: 'list', values:['jhop', 'jlkj']}, () => {}); */
+    f3.color({name: 'color', value:'#ff0000'}, () => {});
+    f3.vector2({name: 'vector2', 
+        x: { obj: data2, prop: 'x', min: 0, max: 1},
+        y: { obj: data2, prop: 'y', min: 0, max: 1},
+    }, () => {});
+    f3.folder({name: 'okok'});
+//}
