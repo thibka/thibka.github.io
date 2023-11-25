72fb6bfab38833827be1c4679b19e5e627236620
Thibaut Foussard <thibaut.foussard@tmrw.com>
2023-11-25 16:22:59 +0800

    perfect-gui image example update

--- perfect-gui/examples/src/js/methods/image.js
+++ perfect-gui/examples/src/js/methods/image.js
@@ -11,46 +11,33 @@ export default function image() {
 
     gui.image({ name: 'Image 1',
         path: 'https://raw.githubusercontent.com/thibka/thibka.github.io/master/perfect-gui/_data/img/DALL·E-2022-11-13-20.11.16---portrait-of-a-squirrel-in-an-officier-suit,-style-of-a-Rembrandt-painting.jpg'},
-        evt => {
-            element.style.backgroundImage = `url( ${evt.path} )`;
-        }
+        changeBackground
     );
 
     gui.image({ 
-            name: 'Image 2',
-            path: 'https://raw.githubusercontent.com/thibka/thibka.github.io/master/perfect-gui/_data/img/DALL·E-2022-11-13-20.13.55---1-blonde-haired-girl-with-her-orange-cat,-watching-the-whales-in-Tadoussac,-Canada.-In-the-style-of-an-oil-painting..jpg',
-            selected: true
-        },
-        evt => {
-            element.style.backgroundImage = `url( ${evt.path} )`;
-        }
-    );
+        name: 'Image 2',
+        path: 'https://raw.githubusercontent.com/thibka/thibka.github.io/master/perfect-gui/_data/img/DALL·E-2022-11-13-20.13.55---1-blonde-haired-girl-with-her-orange-cat,-watching-the-whales-in-Tadoussac,-Canada.-In-the-style-of-an-oil-painting..jpg',
+        selected: true
+    }, changeBackground);
 
     gui.image({ 
-            name: 'Image 3',
-            path: 'https://raw.githubusercontent.com/thibka/thibka.github.io/master/perfect-gui/_data/img/DALL·E-2022-11-13-20.11.52---a-blonde-girl-riding-a-whale-in-the-style-of-hopper.jpg',
-        },
-        evt => {
-            element.style.backgroundImage = `url( ${evt.path} )`;
-        }
-    );
+        name: 'Image 3',
+        path: 'https://raw.githubusercontent.com/thibka/thibka.github.io/master/perfect-gui/_data/img/DALL·E-2022-11-13-20.11.52---a-blonde-girl-riding-a-whale-in-the-style-of-hopper.jpg',
+    }, changeBackground);
 
     gui.image({ 
         name: 'Image 4',
         path: 'https://raw.githubusercontent.com/thibka/thibka.github.io/master/perfect-gui/_data/img/DALL·E-2023-11-25-16.00.40---An-oil-painting-of-a-short-haired-orange-and-white-alley-cat.jpg',
-    },
-    evt => {
-        element.style.backgroundImage = `url( ${evt.path} )`;
-    });
+    }, changeBackground);
 
     gui.image({ 
         name: 'Image 5',
         path: 'https://raw.githubusercontent.com/thibka/thibka.github.io/master/perfect-gui/_data/img/DALL·E-2023-11-25-16.05.33---An-image-in-the-style-of-a-1900s-painter,-depicting-a-blonde-girl-riding-a-whale.jpg',
-    },
-    evt => {
+    }, changeBackground);
+
+    function changeBackground(evt) {
         element.style.backgroundImage = `url( ${evt.path} )`;
-    });
-    
+    }
 }
 
     
