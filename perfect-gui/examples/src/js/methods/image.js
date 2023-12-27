022e071328bea15d3f0c284ee0a47237d1548012
Thibaut Foussard <thibaut.foussard@tmrw.com>
2023-12-27 13:41:09 +0300

    pgui: remove useless object binding on image()

--- perfect-gui/examples/src/js/methods/image.js
+++ perfect-gui/examples/src/js/methods/image.js
@@ -35,14 +35,6 @@ export default function image() {
         path: 'https://raw.githubusercontent.com/thibka/thibka.github.io/master/perfect-gui/_data/img/DALL·E-2023-11-25-16.05.33---An-image-in-the-style-of-a-1900s-painter,-depicting-a-blonde-girl-riding-a-whale.jpg',
     }, changeBackground);
 
-    const state = { selected: false };
-    gui.image({ 
-        name: 'Image 6',
-        path: 'https://raw.githubusercontent.com/thibka/thibka.github.io/master/perfect-gui/_data/img/DALL·E-2023-11-25-16.05.33---An-image-in-the-style-of-a-1900s-painter,-depicting-a-blonde-girl-riding-a-whale.jpg',
-        obj: state,
-        prop: 'selected'
-    }, changeBackground);
-
     function changeBackground(evt) {
         element.style.backgroundImage = `url( ${evt.path} )`;
     }
