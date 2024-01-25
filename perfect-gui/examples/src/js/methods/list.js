ab6d635a4b8c75765bb82d9a77830ab4cb69bbf1
Thibaut Foussard <thibaut.foussard@tmrw.com>
2024-01-25 15:08:05 +0100

    Pgui list() object values update

--- perfect-gui/examples/src/js/methods/list.js
+++ perfect-gui/examples/src/js/methods/list.js
@@ -9,16 +9,32 @@ export default function list() {
     });
 
     // Approach 1, using a callback function
-    gui.list({ name: 'List (value & callback)', values: ['red', 'pink', 'yellow', 'blue'], value: 1 }, selected_value => {
+    gui.list({ name: 'Value & callback', values: ['red', 'pink', 'yellow', 'blue'], value: 0 }, selected_value => {
         element.style.backgroundColor = selected_value;
     });
 
-    // Approach 2, using object binding
+    // Approach 2, using object binding with an array of strings
     const values = ['red', 'pink', 'yellow', 'blue'];
-    const color = { value: 2 };
-    gui.list({ name: 'List (object binding)', values, obj: color, prop: 'value' }, 
+    const color = { value: 0 };
+    gui.list({ name: 'Object binding (strings)', values, obj: color, prop: 'value' }, 
         (value, index) => {
             element.style.backgroundColor = value;
+        }
+    );
+
+    // Approach 3, using object binding with an array of objects
+    // The "name" property is only used to control what's displayed in the option list
+    // The intrinsec value of each item is the "value" property
+    const color2 = { value: '#993333' };
+    const objectValues = [
+        {name: 'reddish', value: '#993333'}, 
+        {name: 'pinkish', value: '#aa33aa'}, 
+        {name: 'yellowish', value: '#999933'}, 
+        {name: 'blueish', value: '#333399'}
+    ];
+    gui.list({ name: 'Object binding (objects)', values: objectValues, obj: color2, prop: 'value' }, 
+        (obj, index) => {
+            element.style.backgroundColor = obj.value;
         }
     );
 }
